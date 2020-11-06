80a0f74e5f3d8d68bda1b5ed411c38c599e021b3
sanjay1708 <54763695+sanjay1708@users.noreply.github.com>
2020-11-06 13:59:54 +0530

    commit corrected

--- logout.php
+++ logout.php
@@ -1,6 +1,6 @@
-<?php // line 1 added to enable color highlight
+<?php 
 
 session_start();
 unset($_SESSION['roll_number']);
 unset($_SESSION['user_name']);
-header('Location: index.php');
+header('Location: login.php');
